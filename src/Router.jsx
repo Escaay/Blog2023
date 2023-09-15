52cd5864cd28de91165c6cc464a8418e9f7e1d82
@qiuwenjing <qiuwenjing@rt-thread.com>
2023-09-15 16:41:54 +0800

    路由

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useRef } from 'react'
+import { useState } from 'react'
 import Home from './pages/home'
-import './router.less'
 const Career = React.lazy(() => import('./pages/career'))
 const Travel = React.lazy(() => import('./pages/travel'))
 const Hobby = React.lazy(() => import('./pages/hobby'))
@@ -10,20 +9,19 @@ const Works = React.lazy(() => import('./pages/works'))
 import { Routes, Route, Outlet } from 'react-router-dom'
 
 function Router() {
-	return (
-		<>
-		{/* <RouterTransition ref1={ref1} ref2={ref2} class1="class1" class2="class2"> */}
-			<Routes>
-				<Route path='/' element={<Home />}></Route>
-				<Route path='/career' element={<Career />}></Route>
-				<Route path='/travel' element={<Travel />}></Route>
-				<Route path='/hobby' element={<Hobby />}></Route>
-				<Route path='/recent' element={<Recent />}></Route>
-				<Route path='/works' element={<Works />}></Route>
-			</Routes>
-		{/* </RouterTransition> */}
-		</>
-	)
+
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/career' element={<Career />}></Route>
+        <Route path='/travel' element={<Travel />}></Route>
+        <Route path='/hobby' element={<Hobby />}></Route>
+        <Route path='/recent' element={<Recent />}></Route>
+        <Route path='/works' element={<Works />}></Route>
+      </Routes>
+    </>
+  )
 }
 
 export default Router
